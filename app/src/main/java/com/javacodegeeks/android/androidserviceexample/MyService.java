ec1bf7ca46b18152c97ea9c265c32c458f1a74c0
rligocki <romanligocki@Romek-MacBookAir.lan>
2015-04-15 10:16:52 +0200

    Creating repository
    First commit

---
+++ app/src/main/java/com/javacodegeeks/android/androidserviceexample/MyService.java
@@ -0,0 +1,128 @@
+package com.javacodegeeks.android.androidserviceexample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class MyService extends Service {
+	public MyService() {
+	}
+    static String UDP_BROADCAST = "UDPBroadcast";
+
+    //Boolean shouldListenForUDPBroadcast = false;
+    DatagramSocket socket;
+
+    private void listenAndWaitAndThrowIntent(Integer port) throws Exception {
+        byte[] recvBuf = new byte[15000];
+        if (socket == null || socket.isClosed()) {
+            socket = new DatagramSocket(port);
+            socket.setBroadcast(true);
+        }
+        //socket.setSoTimeout(1000);
+        DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+        Log.e("UDP", "Waiting for UDP broadcast");
+        socket.receive(packet);
+
+        String senderIP = packet.getAddress().getHostAddress();
+        String message = new String(packet.getData()).trim();
+
+        Log.e("UDP", "Got UDB broadcast from " + senderIP + ", message: " + message);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String MyText = "UDP receiver";
+        Notification mNotification = new Notification(R.drawable.ic_launcher, MyText, System.currentTimeMillis() );
+        //The three parameters are: 1. an icon, 2. a title, 3. time when the notification appears
+
+        String MyNotificationTitle = "Packet received!";
+        String MyNotificationText  = senderIP.toString() + ", message: " + message.toString() ;
+        Intent MyIntent = new Intent(Intent.ACTION_VIEW);
+        PendingIntent StartIntent = PendingIntent.getActivity(getApplicationContext(),0,MyIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        //A PendingIntent will be fired when the notification is clicked. The FLAG_CANCEL_CURRENT flag cancels the pendingintent
+
+        mNotification.setLatestEventInfo(getApplicationContext(), MyNotificationTitle, MyNotificationText, StartIntent);
+        mNotification.defaults |= Notification.DEFAULT_SOUND;
+        int NOTIFICATION_ID = 1;
+        notificationManager.notify(NOTIFICATION_ID , mNotification);
+
+        broadcastIntent(senderIP, message);
+        socket.close();
+    }
+
+    private void broadcastIntent(String senderIP, String message) {
+        Intent intent = new Intent(MyService.UDP_BROADCAST);
+        intent.putExtra("sender", senderIP);
+        intent.putExtra("message", message);
+        sendBroadcast(intent);
+    }
+
+    Thread UDPBroadcastThread;
+
+    void startListenForUDPBroadcast() {
+        UDPBroadcastThread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Integer port = 3000;
+                    while (shouldRestartSocketListen) {
+                        listenAndWaitAndThrowIntent( port);
+                    }
+                    //if (!shouldListenForUDPBroadcast) throw new ThreadDeath();
+                } catch (Exception e) {
+                    Log.i("UDP", "no longer listening for UDP broadcasts cause of error " + e.getMessage());
+                }
+            }
+        });
+        UDPBroadcastThread.start();
+    }
+
+    private Boolean shouldRestartSocketListen=true;
+
+    void stopListen() {
+        shouldRestartSocketListen = false;
+        socket.close();
+    }
+    
+    
+    
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	
+	@Override
+    public void onCreate() {
+        Toast.makeText(this, "The new Service was Created", Toast.LENGTH_LONG).show();
+
+
+
+    }
+ 
+    @Override
+    public void onStart(Intent intent, int startId) {
+    	// For time consuming an long tasks you can launch a new thread here...
+        Toast.makeText(this, " Service Started", Toast.LENGTH_LONG).show();
+
+        shouldRestartSocketListen = true;
+        startListenForUDPBroadcast();
+        Log.i("UDP", "Service started");
+
+    }
+ 
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+
+        stopListen();
+    }
+}
